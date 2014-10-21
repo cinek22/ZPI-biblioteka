6a0ed58206d4bdba881a82fafac21f26aee76428
raphau90 <rafciiu@gmail.com>
2014-10-21 06:13:12 +0200

    O ile dobrze zrobiłem to powinny być widoczne zmiany Tomka

---
+++ BibliotekaMobilnaPWR/src/com/example/bibliotekamobilnapwr/ParseURLActivity.java
@@ -0,0 +1,88 @@
+package com.example.bibliotekamobilnapwr;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ParseURLActivity extends Activity{
+	
+	private EditText mTitle;
+	private EditText mAuthor;
+	private Button mSearch;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.logowanie);
+		setupView();
+		setupListeners();
+	}
+	
+	private void setupView(){
+		mTitle = (EditText) findViewById(R.id.main_title_et);
+		mAuthor = (EditText) findViewById(R.id.main_author_et);
+		mSearch  = (Button) findViewById(R.id.main_search);
+	}
+	
+	private void setupListeners(){
+		mSearch.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			if(!mTitle.getText().toString().equals("")
+						&& !mAuthor.getText().toString().equals("")){
+				
+				String siteUrl = (StringsAndLinks.SEARCH_TITLE_NOLOGGED+mTitle+StringsAndLinks.SEARCH_AUTHOR_NOLOGGED+mAuthor+StringsAndLinks.SEARCH_END_NOLOGGED);
+	            ( new ParseURL() ).execute(new String[]{siteUrl});				
+			}else{
+					Toast.makeText(ParseURLActivity.this, "Obydwa pola musz¹ byæ wype³nione", Toast.LENGTH_LONG).show();
+				}
+				
+			}
+		});
+	}
+	
+	public class ParseURL extends AsyncTask<String, Void, String> {
+		
+		@Override 
+	    protected String doInBackground(String... strings) {
+			
+	        StringBuffer buffer = new StringBuffer();
+	        try {
+	            Document doc  = Jsoup.connect(strings[0]).get();
+	            Elements metaElems = doc.select("class=td1");
+	            buffer.append("DATA\r\n");
+	            for (Element metaElem : metaElems) {
+	            	String lp = metaElem.attr("lp");
+	            	String selector = metaElem.attr("selector");
+	                String author = metaElem.attr("author");
+	                String title = metaElem.attr("title");
+	                String egzemplarzy = metaElem.attr("egzemplarzy");
+	                String adresElektroniczny = metaElem.attr("adresElektroniczny");
+	                buffer.append("lp ["+lp+"] - selector ["+selector+"] - author  ["+author+"] - title ["+title+"] - egzemplarzy ["+egzemplarzy+"] - adresElektroniczny ["+adresElektroniczny+"] \r\n");
+	            }
+	        }
+	        catch(Throwable t) {
+	            t.printStackTrace();
+	        }
+
+	        System.out.print(buffer.toString());
+
+	        //Mo¿na to zwracaæ na jakimœ edittext
+	        //jednak lepiej konwertowaæ do xml-a
+	        return buffer.toString();
+	    }
+	
+}}
--- BibliotekaMobilnaPWR/src/com/example/bibliotekamobilnapwr/StringsAndLinks.java
+++ BibliotekaMobilnaPWR/src/com/example/bibliotekamobilnapwr/StringsAndLinks.java
@@ -2,4 +2,7 @@ package com.example.bibliotekamobilnapwr;
 
 public class StringsAndLinks {
 	public static final String LOGIN_ADDRESS = "http://aleph.bg.pwr.wroc.pl/F/2P8IMXMCB87Q1KVEU3KCAF2NRYPGSPA7PDJA912YCDBG4SVP3G-00753?func=file&file_name=login-session";
+	public static final String SEARCH_TITLE_NOLOGGED = "http://aleph.bg.pwr.wroc.pl/F/?func=find-a&find_code=WSU&request=";
+	public static final String SEARCH_AUTHOR_NOLOGGED = "&request_op=AND&find_code=WAU&request=";
+	public static final String SEARCH_END_NOLOGGED = "&request_op=AND&find_code=TIT&request=&request_op=AND&find_code=WTI&request=&request_op=AND&find_code=WYR&request=&request_op=AND&find_code=WPU&request=&adjacent=N&local_base=TUR&x=0&y=0&filter_code_1=WLN&filter_request_1=&filter_code_2=WYR&filter_request_2=&filter_code_3=WYR&filter_request_3=&filter_code_4=WFT&filter_request_4=&filter_code_5=WSB&filter_request_5=";
 }
