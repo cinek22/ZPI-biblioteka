74d8c416e603d1583af4d9b974f6e4a1fc80ac7a
Tomek <tomasz_luczak@o2.pl>
2014-11-03 10:26:19 +0100

    Update search book

--- BibliotekaMobilnaPWR/src/com/example/bibliotekamobilnapwr/ParseURLActivity.java
+++ BibliotekaMobilnaPWR/src/com/example/bibliotekamobilnapwr/ParseURLActivity.java
@@ -1,7 +1,6 @@
 package com.example.bibliotekamobilnapwr;
 
 import java.io.StringWriter;
-import java.util.List;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.Transformer;
@@ -13,21 +12,15 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.renderscript.Sampler.Value;
-import android.text.Html;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TableRow.LayoutParams;
@@ -36,7 +29,10 @@ import android.widget.Toast;
 
 public class ParseURLActivity extends Activity {
 
+	public static final String GREEN_1       = "#7FA016";
+	public static final String GREEN_2       = "#3F5300";
 	TableLayout table_layout;
+	TableLayout table;
 	private Button btnBack;
 	ParseURL parseURL = new ParseURL();
 
@@ -48,11 +44,11 @@ public class ParseURLActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.result_book_nolog);
 		setupView();
-		
+
 		Intent intent = getIntent();
-	    String message = intent.getStringExtra("URL");
-	    parseURL.execute(message);
-	    
+		String message = intent.getStringExtra("URL");
+		parseURL.execute(message);
+
 		btnBack.setOnClickListener(new View.OnClickListener() {
 
 			@Override
@@ -64,14 +60,11 @@ public class ParseURLActivity extends Activity {
 	}
 
 	private void setupView() {
-		/*mWynik = (TextView) findViewById(R.id.resultNoLog);
-		mWynik.setMovementMethod(new ScrollingMovementMethod());*/
 		table_layout = (TableLayout) findViewById(R.id.tableLayout1);
+		table = (TableLayout) findViewById(R.id.table);
 		btnBack = (Button) findViewById(R.id.btnBack);
 
 	}
-	
-	
 
 	public class ParseURL extends AsyncTask<Void, Void, Void> {
 
@@ -97,108 +90,146 @@ public class ParseURLActivity extends Activity {
 		@Override
 		protected Void doInBackground(Void... params) {
 			try {
-				//Document jsoupe 
+				// Document jsoupe
 				Document document = Jsoup.connect(URL).get();
-				Elements description2 = document.select("body table#short_table tr[valign=baseline]");
-				
+				Elements description2 = document
+						.select("body table#short_table tr[valign=baseline]");
+
 				createXML(description2);
-				
+
 			} catch (Exception e) {
 				e.printStackTrace();
-				Toast.makeText(ParseURLActivity.this, "buuuu", Toast.LENGTH_LONG).show();
+				Toast.makeText(ParseURLActivity.this, "B³¹d po³¹czenia",
+						Toast.LENGTH_LONG).show();
 			}
-			
+
 			mProgressDialog.dismiss();
 			return null;
 		}
-		
-		public void createXML (Elements description2) throws Exception{
-			
-			//count quantity book
-			int quantityBook = 0;
-			
-			//XML
-			org.w3c.dom.Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-		    // create root: <search>
-			org.w3c.dom.Element root = doc.createElement("search");
-		    doc.appendChild(root);
-		    
-		    for (Element desc : description2){
-		    // create: <book>
-		    org.w3c.dom.Element book = doc.createElement("book");
-		    root.appendChild(book);
-		    // add attr: id =
-		    book.setAttribute("book", desc.select("td[valign=top]").get(0).text());
-			quantityBook++;
-		    
-		    // create: <author>
-		    org.w3c.dom.Element author = doc.createElement("author");
-		    book.appendChild(author);
-		    author.setTextContent(desc.select("td[valign=top]").get(2).text());
 
-		    // create: <title>
-		    org.w3c.dom.Element title = doc.createElement("title");
-		    book.appendChild(title);
-		    title.setTextContent(desc.select("td[valign=top]").get(3).text() + desc.select("td[valign=top]").get(4).text());
-			
-		    // create: <availability>
-		    org.w3c.dom.Element availibility = doc.createElement("availibility");
-		    book.appendChild(availibility);
-		    availibility.setTextContent(desc.select("td[valign=top]").get(5).text());
-		    }
-		    
-		    // create Transformer object
-		    Transformer transformer = TransformerFactory.newInstance().newTransformer();
-		    StringWriter writer = new StringWriter();
-		    StreamResult result = new StreamResult(writer);
-		    transformer.transform(new DOMSource(doc), result);
-		    createTable(quantityBook, doc);
+		public void createXML(Elements description2) throws Exception {
+
+			// count quantity book
+			int quantityBook = 0;
+
+			// XML
+			org.w3c.dom.Document doc = DocumentBuilderFactory.newInstance()
+					.newDocumentBuilder().newDocument();
+			// create root: <search>
+			org.w3c.dom.Element root = doc.createElement("search");
+			doc.appendChild(root);
+
+			for (Element desc : description2) {
+				// create: <book>
+				org.w3c.dom.Element book = doc.createElement("book");
+				root.appendChild(book);
+				// add attr: id =
+				book.setAttribute("book", desc.select("td[valign=top]").get(0)
+						.text());
+				quantityBook++;
+
+				// create: <author>
+				org.w3c.dom.Element author = doc.createElement("author");
+				book.appendChild(author);
+				author.setTextContent(desc.select("td[valign=top]").get(2)
+						.text());
+
+				// create: <title>
+				org.w3c.dom.Element title = doc.createElement("title");
+				book.appendChild(title);
+				title.setTextContent(desc.select("td[valign=top]").get(3)
+						.text()
+						+ desc.select("td[valign=top]").get(4).text());
+
+				// create: <availability>
+				org.w3c.dom.Element availibility = doc
+						.createElement("availibility");
+				book.appendChild(availibility);
+				availibility.setTextContent(desc.select("td[valign=top]")
+						.get(5).text());
+			}
+
+			// create Transformer object
+			Transformer transformer = TransformerFactory.newInstance()
+					.newTransformer();
+			StringWriter writer = new StringWriter();
+			StreamResult result = new StreamResult(writer);
+			transformer.transform(new DOMSource(doc), result);
+			createTable(quantityBook, doc);
 		}
-		
 
 	}
 
-	 private void createTable(int quantityBook, org.w3c.dom.Document doc) {
+	private void createTable(int quantityBook, org.w3c.dom.Document doc) {
 
-		
-		  for (int i = 0; i < quantityBook; i++) {
+		TableRow rowMenu = new TableRow(this);
+		TextView menuAuthor = new TextView(this);
+		menuAuthor.setLayoutParams(new LayoutParams(60,
+				LayoutParams.WRAP_CONTENT));
+		menuAuthor.setText("Autor");
+		menuAuthor.setTextSize(18);
+		rowMenu.addView(menuAuthor);
 
-		  TableRow row = new TableRow(this); 
-		  /*row.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
-				     LayoutParams.WRAP_CONTENT));*/
+		TextView menuTitle = new TextView(this);
+		menuTitle.setLayoutParams(new LayoutParams(60,
+				LayoutParams.WRAP_CONTENT));
+		menuTitle.setText("Tytu³");
+		menuTitle.setTextSize(18);
+		rowMenu.addView(menuTitle);
 
-		  	//author
+		TextView menuAvailibility = new TextView(this);
+		menuAvailibility.setLayoutParams(new LayoutParams(60,
+				LayoutParams.WRAP_CONTENT));
+		menuAvailibility.setText("Dostêpnoœæ");
+		menuAvailibility.setTextSize(18);
+		rowMenu.addView(menuAvailibility);
+
+		table.addView(rowMenu);
+
+		for (int i = 0; i < quantityBook; i++) {
+			String c;
+			if (i == 0 || i % 2 == 0) {
+				c = GREEN_1;
+			} else {
+				c = GREEN_2;
+			}
+
+			TableRow row = new TableRow(this);
+			/*
+			 * row.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+			 * LayoutParams.WRAP_CONTENT));
+			 */
+
+			// author
 			TextView tvAuthor = new TextView(this);
-			tvAuthor.setLayoutParams(new LayoutParams( 60,
-				      LayoutParams.WRAP_CONTENT));
+			tvAuthor.setLayoutParams(new LayoutParams(60,
+					LayoutParams.WRAP_CONTENT));
 			tvAuthor.setText(doc.getElementsByTagName("author").item(i)
 					.getTextContent());
+			tvAuthor.setTextColor(Color.parseColor(c));
 			row.addView(tvAuthor);
-			
+
 			// title
 			TextView tvTitle = new TextView(this);
 			tvTitle.setLayoutParams(new LayoutParams(60,
-				      LayoutParams.WRAP_CONTENT));
+					LayoutParams.WRAP_CONTENT));
 			tvTitle.setText(doc.getElementsByTagName("title").item(i)
 					.getTextContent());
+			tvTitle.setTextColor(Color.parseColor(c));
 			row.addView(tvTitle);
 
-			//available
-			TextView tvAvailable = new TextView(this);
-			tvAvailable.setLayoutParams(new LayoutParams(60,
-				      LayoutParams.WRAP_CONTENT));
-			tvAvailable.setText(doc.getElementsByTagName("availibility").item(i)
-					.getTextContent());
-			row.addView(tvAvailable);
-			
+			// availibility
+			TextView tvAvailibility = new TextView(this);
+			tvAvailibility.setLayoutParams(new LayoutParams(60,
+					LayoutParams.WRAP_CONTENT));
+			tvAvailibility.setText(doc.getElementsByTagName("availibility")
+					.item(i).getTextContent());
+			tvAvailibility.setTextColor(Color.parseColor(c));
+			row.addView(tvAvailibility);
 
+			table_layout.addView(row);
 
-		   table_layout.addView(row);
+		}
+	}
 
-		  }
-		 }
-
-	 
-//	 tv.setText(doc.getElementsByTagName("author").item(0).getTextContent());
-	 
 }
